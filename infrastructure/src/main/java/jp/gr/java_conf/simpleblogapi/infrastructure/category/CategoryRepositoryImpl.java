a4045a8f2922538a35e80fb4d17f1c0108c27b47
masahosono <dev.masahosono@gmail.com>
2021-08-09 01:12:41 +0900

    Remove log4j

--- infrastructure/src/main/java/jp/gr/java_conf/simpleblogapi/infrastructure/category/CategoryRepositoryImpl.java
+++ infrastructure/src/main/java/jp/gr/java_conf/simpleblogapi/infrastructure/category/CategoryRepositoryImpl.java
@@ -5,12 +5,10 @@ import jp.gr.java_conf.simpleblogapi.domain.category.CategoryList;
 import jp.gr.java_conf.simpleblogapi.domain.category.CategoryRepository;
 import jp.gr.java_conf.simpleblogapi.infrastructure.category.exception.CategoryDatabaseException;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
-@Slf4j
 @Repository
 @RequiredArgsConstructor
 public class CategoryRepositoryImpl implements CategoryRepository {
@@ -27,7 +25,6 @@ public class CategoryRepositoryImpl implements CategoryRepository {
                     jdbcTemplate.query(query, new BeanPropertyRowMapper<>(CategoryEntity.class));
             return categoryListFactory.from(categories);
         } catch(RuntimeException exception) {
-            log.info(exception.getMessage());
             throw new CategoryDatabaseException("Failed to connect the category table.");
         }
     }
