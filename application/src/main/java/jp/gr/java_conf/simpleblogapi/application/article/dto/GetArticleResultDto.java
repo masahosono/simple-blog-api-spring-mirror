cc9e1c5cfe3371a569aea4c2591d49065a0aaccf
masahosono <dev.masahosono@gmail.com>
2021-08-13 00:17:38 +0900

    Optimize Imports

--- application/src/main/java/jp/gr/java_conf/simpleblogapi/application/article/dto/GetArticleResultDto.java
+++ application/src/main/java/jp/gr/java_conf/simpleblogapi/application/article/dto/GetArticleResultDto.java
@@ -1,6 +1,5 @@
 package jp.gr.java_conf.simpleblogapi.application.article.dto;
 
-import jp.gr.java_conf.simpleblogapi.domain.article.ArticleList;
 import jp.gr.java_conf.simpleblogapi.domain.article.ResolvedArticleList;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
