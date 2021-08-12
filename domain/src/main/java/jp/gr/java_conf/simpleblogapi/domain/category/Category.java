3a37e58d1912fc3a1b98c8ae0575aea7d7bd1582
masahosono <dev.masahosono@gmail.com>
2021-08-13 00:51:31 +0900

    不要なメソッドを削除

--- domain/src/main/java/jp/gr/java_conf/simpleblogapi/domain/category/Category.java
+++ domain/src/main/java/jp/gr/java_conf/simpleblogapi/domain/category/Category.java
@@ -11,12 +11,4 @@ public class Category {
     private final String name;
     private final Integer parentId;
 
-    public ResolvedCategory resolve() {
-        return ResolvedCategory.of(
-                id,
-                name,
-                0, // TODO: 記事の個数の指定
-                parentId
-        );
-    }
 }
--- domain/src/main/java/jp/gr/java_conf/simpleblogapi/domain/category/ResolvedCategoryList.java
+++ domain/src/main/java/jp/gr/java_conf/simpleblogapi/domain/category/ResolvedCategoryList.java
@@ -1,21 +1,13 @@
 package jp.gr.java_conf.simpleblogapi.domain.category;
 
 import java.util.List;
-import java.util.stream.Collectors;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-import org.springframework.util.StringUtils;
 
 @RequiredArgsConstructor(staticName = "of")
 @Getter
 public class ResolvedCategoryList {
 
     private final List<ResolvedCategory> categoryList;
-
-    public List<ResolvedCategory> getCategoriesByParentId(int categoryId) {
-        return categoryList.stream()
-                .filter(category -> !StringUtils.isEmpty(category.getParentId()) && category.getParentId() == categoryId)
-                .collect(Collectors.collectingAndThen(Collectors.toList(), List::copyOf));
-    }
 
 }
