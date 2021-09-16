146825362d218d408627a08dd9462d959d284622
masahosono <dev.masahosono@gmail.com>
2021-09-17 01:41:46 +0900

    カテゴリ登録のクエリを実装

--- infrastructure/src/main/java/jp/gr/java_conf/simpleblogapi/infrastructure/category/CategoryRepositoryImpl.java
+++ infrastructure/src/main/java/jp/gr/java_conf/simpleblogapi/infrastructure/category/CategoryRepositoryImpl.java
@@ -1,6 +1,7 @@
 package jp.gr.java_conf.simpleblogapi.infrastructure.category;
 
 import java.util.List;
+import jp.gr.java_conf.simpleblogapi.domain.category.Category;
 import jp.gr.java_conf.simpleblogapi.domain.category.CategoryList;
 import jp.gr.java_conf.simpleblogapi.domain.category.CategoryRepository;
 import jp.gr.java_conf.simpleblogapi.infrastructure.category.exception.CategoryDatabaseException;
@@ -16,6 +17,7 @@ public class CategoryRepositoryImpl implements CategoryRepository {
     private final JdbcTemplate jdbcTemplate;
 
     private final CategoryListFactory categoryListFactory;
+    private final CategoryFactory categoryFactory;
 
     public CategoryList getCategory() {
         String query = "SELECT * FROM category";
@@ -25,6 +27,34 @@ public class CategoryRepositoryImpl implements CategoryRepository {
                     jdbcTemplate.query(query, new BeanPropertyRowMapper<>(CategoryEntity.class));
             return categoryListFactory.from(categories);
         } catch(RuntimeException exception) {
+            throw new CategoryDatabaseException("Failed to connect the category table.");
+        }
+    }
+
+    public Category getCategoryById(int id) {
+        String query = "SELECT * FROM category WHERE id = ?";
+
+        try {
+            CategoryEntity category =
+                    jdbcTemplate.queryForObject(query, new BeanPropertyRowMapper<>(CategoryEntity.class), id);
+            return categoryFactory.from(category);
+        } catch(RuntimeException exception) {
+            throw new CategoryDatabaseException("Failed to connect the category table.");
+        }
+    }
+
+    public int registerCategory(String name, Integer parentId) {
+        String query = "INSERT INTO " +
+                "category(name, parent_id) " +
+                "VALUES(?,?)";
+
+        try {
+            jdbcTemplate.update(
+                    query,
+                    name,
+                    parentId);
+            return jdbcTemplate.queryForObject("SELECT LAST_INSERT_ID()", Integer.class);
+        } catch (RuntimeException exception) { // TODO: SQLIntegrityConstraintViolationExceptionをキャッチしたい
             throw new CategoryDatabaseException("Failed to connect the category table.");
         }
     }
