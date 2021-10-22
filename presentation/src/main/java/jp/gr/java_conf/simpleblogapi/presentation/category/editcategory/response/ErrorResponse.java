b28c31b1e1c78bd228e308264816d17d5d7388f2
masahosono <dev.masahosono@gmail.com>
2021-10-23 01:32:21 +0900

    NotNullアノテーションを削除

--- presentation/src/main/java/jp/gr/java_conf/simpleblogapi/presentation/category/editcategory/response/ErrorResponse.java
+++ presentation/src/main/java/jp/gr/java_conf/simpleblogapi/presentation/category/editcategory/response/ErrorResponse.java
@@ -1,13 +1,11 @@
 package jp.gr.java_conf.simpleblogapi.presentation.category.editcategory.response;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import javax.validation.constraints.NotNull;
 import lombok.Builder;
 
 @Builder
 public class ErrorResponse {
 
-    @NotNull
     @JsonProperty("message")
     private final String message;
 }
