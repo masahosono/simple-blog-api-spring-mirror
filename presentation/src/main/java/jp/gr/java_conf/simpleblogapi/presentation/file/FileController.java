f2599ccc8300455d8d6bda38ee8301fd336fed24
masahosono <dev.masahosono@gmail.com>
2021-11-15 00:41:53 +0900

    ファイルアップロードAPIのControllerからロガーを削除

--- presentation/src/main/java/jp/gr/java_conf/simpleblogapi/presentation/file/FileController.java
+++ presentation/src/main/java/jp/gr/java_conf/simpleblogapi/presentation/file/FileController.java
@@ -20,7 +20,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-@Slf4j
 @RestController
 @Component
 @RequiredArgsConstructor
@@ -36,10 +35,6 @@ public class FileController {
             @RequestParam MultipartFile multipartFile) {
 
         LocalDateTime localDateTime = LocalDateTime.now();
-
-        log.info("ContentType:　"  +  multipartFile.getContentType());
-        log.info("OriginalFileName: " + multipartFile.getOriginalFilename());
-        log.info("Size: " + String.valueOf(multipartFile.getSize()));
 
         RegisterFileArgsDto registerFileArgsDto =
                 registerFileArgsDtoFactory.factory(multipartFile, localDateTime);
