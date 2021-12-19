a80d17a638f3b6d7cd355e96c5823f544969df45
masahosono <dev.masahosono@gmail.com>
2021-12-20 00:56:12 +0900

    ユーザログインAPIのIFを作成

--- presentation/src/main/java/jp/gr/java_conf/simpleblogapi/presentation/user/UserController.java
+++ presentation/src/main/java/jp/gr/java_conf/simpleblogapi/presentation/user/UserController.java
@@ -5,6 +5,7 @@ import jp.gr.java_conf.simpleblogapi.presentation.user.signup.request.UserSignup
 import jp.gr.java_conf.simpleblogapi.presentation.user.signup.request.factory.UserSignupArgsDtoFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +17,11 @@ public class UserController {
 
     private final UserSignupService userSignupService;
     private final UserSignupArgsDtoFactory userSignupArgsDtoFactory;
+
+    @GetMapping(value = "/api/user/auth", produces = "application/json")
+    public void auth() {
+        // TODO: 処理を記載
+    }
 
     @PostMapping(value = "/api/user/signup", produces = "application/json")
     public void signup(
