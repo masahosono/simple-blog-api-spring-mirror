23c7b3c43933de273a05eb854f6de2ddc4fb6271
masahosono <dev.masahosono@gmail.com>
2022-01-09 19:51:22 +0900

    OpenAPIを導入

---
+++ src/main/java/jp/gr/java_conf/simpleblogapi/OpenApiConfig.java
@@ -0,0 +1,18 @@
+package jp.gr.java_conf.simpleblogapi;
+
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI restApi() {
+        return new OpenAPI()
+                .info(new Info().title("").description(""))
+                .externalDocs(new ExternalDocumentation().description("").url(""));
+    }
+}
