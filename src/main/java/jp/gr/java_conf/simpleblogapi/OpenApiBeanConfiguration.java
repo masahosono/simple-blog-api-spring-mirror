73f2535619652206ba662e8f81232a52e51976d2
masahosono <dev.masahosono@gmail.com>
2022-01-09 19:59:19 +0900

    Rename OpenApiBeanConfiguration

--- src/main/java/jp/gr/java_conf/simpleblogapi/OpenApiBeanConfiguration.java
+++ src/main/java/jp/gr/java_conf/simpleblogapi/OpenApiBeanConfiguration.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class OpenApiConfig {
+public class OpenApiBeanConfiguration {
 
     @Bean
     public OpenAPI restApi() {
