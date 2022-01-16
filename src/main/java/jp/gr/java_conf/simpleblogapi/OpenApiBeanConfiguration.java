9657579df583bcaa18c7c7d84e3286f052b331f0
masahosono <dev.masahosono@gmail.com>
2022-01-17 01:13:29 +0900

    Update api-docs.json

--- src/main/java/jp/gr/java_conf/simpleblogapi/OpenApiBeanConfiguration.java
+++ src/main/java/jp/gr/java_conf/simpleblogapi/OpenApiBeanConfiguration.java
@@ -1,8 +1,6 @@
 package jp.gr.java_conf.simpleblogapi;
 
-import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,8 +9,6 @@ public class OpenApiBeanConfiguration {
 
     @Bean
     public OpenAPI restApi() {
-        return new OpenAPI()
-                .info(new Info().title("").description(""))
-                .externalDocs(new ExternalDocumentation().description("").url(""));
+        return new OpenAPI();
     }
 }
